7b3b22b9df899be2056b47dbdc4385db6493a903
The Bovine Shaman <contactTheBovineShaman@protonmail.com>
2021-10-16 00:34:46 -0400

    Update quark.js

--- kubejs/server_scripts/mod_specific/quark/quark.js
+++ kubejs/server_scripts/mod_specific/quark/quark.js
@@ -1,11 +1,9 @@
-onEvent('recipes', event => {
-  event.shaped('quark:seed_pouch', [
-    ' A ',
-    'BCB',
-    ' B '
-  ], {
-    A: '#forge:string',
-    B: 'minecraft:leather',
-    C: '#quark:seed_pouch_holdable'
-  })
+onEvent('recipes', e => {
+	e.shaped('quark:seed_pouch', [' A ', 'BCB', ' B '], {
+		A: '#forge:string',
+		B: ['minecraft:leather', 'minecraft:rabbit_hide'],
+		C: '#quark:seed_pouch_holdable'
+	}).id('kubejs:quark/seed_pouch')
+
+	removeRecipeByOutput(e, ['quark:seed_pouch'])
 })
